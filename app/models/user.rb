293de43dd95660085b674dc59c92478968d1d76a
Nicholas <berubenic@gmail.com>
2021-01-10 14:08:13 -0500

    basic navbar layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, :email, presence: true, uniqueness: true
+  has_many :posts
 end
