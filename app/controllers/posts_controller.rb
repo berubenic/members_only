480a0f4cf73b4d878f383815b8cfb959fe220f05
Nicholas <berubenic@gmail.com>
2021-01-11 18:50:55 -0500

    registrations_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
-
+  before_action :authenticate_user!, except: %i[index show]
+  
   # GET /posts
   # GET /posts.json
   def index
